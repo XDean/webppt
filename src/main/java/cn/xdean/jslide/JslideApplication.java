dfadf4708a52133f74eb780a5021bf13da82f26d
Dean Xu <373216024@qq.com>
2020-05-01 18:19:45 +0800

    init project

---
+++ src/main/java/cn/xdean/jslide/JslideApplication.java
@@ -0,0 +1,13 @@
+package cn.xdean.jslide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JslideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JslideApplication.class, args);
+    }
+
+}
