16bd6ebdad84d2bf9000ee52db6f1ad6fb6505b0
Dean Xu <373216024@qq.com>
2020-06-12 22:30:01 +0800

    navigator bar

--- frontend/src/components/element/root.tsx
+++ frontend/src/components/element/root.tsx
@@ -4,6 +4,8 @@ import {Box} from "@material-ui/core";
 import {XElement, XParam, XText} from "../../model/model";
 import {renderChildren, renderElement, renderText} from "../render";
 import PageView from "./page";
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const useStyles = makeStyles({
     root: {
@@ -15,7 +17,31 @@ const useStyles = makeStyles({
         overflow: "hidden",
         backgroundColor: "#cccccc",
         boxSizing: "border-box",
-    }
+    },
+    navigator: {
+        position: "absolute",
+        opacity: 0.7,
+        textDecoration: "none",
+        display: "flex",
+        padding: "4px 6px",
+        borderRadius: "50%",
+        backgroundColor: "#ddd",
+        color: "black",
+        zIndex: 100,
+        "&:hover": {
+            backgroundColor: "#bbb",
+            color: "black",
+        }
+    },
+    navigatorLeft: {
+        left: 5,
+        bottom: 3,
+    },
+    navigatorRight: {
+        left: 50,
+        bottom: 3,
+    },
+    toolBar: {}
 });
 
 type RootProp = {
@@ -60,7 +86,7 @@ const RootView: React.FunctionComponent<RootProp> = (props) => {
                     break;
             }
         });
-    },[]);
+    }, []);
 
     return (
         <Box className={classes.root}>
@@ -69,6 +95,18 @@ const RootView: React.FunctionComponent<RootProp> = (props) => {
                     <PageView key={index} element={e} index={index} total={pages.length} current={current}/>
                 );
             })}
+
+            <Box className={classes.navigator + " " + classes.navigatorLeft} onClick={prevPage}>
+                <KeyboardArrowLeftIcon/>
+            </Box>
+            <Box className={classes.navigator + " " + classes.navigatorRight} onClick={nextPage}>
+                <KeyboardArrowRightIcon/>
+            </Box>
+            <Box className={classes.toolBar}>
+                <a id="page-outline-button" title="Outline"><i className="fa fa-th-large fa-fw"></i></a>
+                <a id="page-fullscreen-button" title="Fullscreen"><i className="fa fa-arrows-alt fa-fw"></i></a>
+                <a id="page-lock-button" title="Lock Toolbar"><i className="fa fa-lock fa-fw"></i></a>
+            </Box>
         </Box>
     )
 };
