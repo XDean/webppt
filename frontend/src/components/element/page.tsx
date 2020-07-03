ca33450ef24771ecd825b5846d68c9504eb237f1
Dean Xu <373216024@qq.com>
2020-07-03 20:02:27 +0800

    page css

--- frontend/src/components/element/page.tsx
+++ frontend/src/components/element/page.tsx
@@ -1,60 +1,10 @@
 import React, {useContext} from 'react';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import {Box, Paper} from "@material-ui/core";
 import {XElement} from "../../model/model";
 import {renderChildren} from "../render";
 import {SlideContext} from "../../model/context";
 import {useProperty} from "../../util/util";
-
-const useStyles = makeStyles<Theme, PageProp>(theme => createStyles({
-    wrapper: {
-        transition: "transform .3s ease",
-    },
-    content: {
-        position: "absolute",
-        width: "100vw",
-        height: "100vh",
-        left: 0,
-        top: 0,
-        padding: "5vh 5vw",
-        backgroundColor: "#fff",
-        overflow: "auto",
-        boxSizing: "border-box",
-    },
-    presentWrapper: {
-        transform: props => {
-            const offset = props.index - props.current;
-            if (offset === 0) {
-                return "translate(0)";
-            } else if (offset === 1) {
-                return "translate(100vw)";
-            } else if (offset === -1) {
-                return "translate(-100vw)";
-            } else if (offset > 0) {
-                return "translate(200vw)";
-            } else {
-                return "translate(-200vw)";
-            }
-        },
-        zIndex: props => props.index - props.current === 0 ? 10 : 5,
-    },
-    outlineWrapper: {
-        position: "relative",
-        display: "inline-block",
-        width: "20vw",
-        height: "20vh",
-        "&:hover": {
-            boxShadow: "0 0 3px 4px #accaff",
-            cursor: "pointer",
-        }
-    },
-    outlineContent: {
-        borderRadius: "2%",
-        transition: "transform .3s ease-out",
-        transform: "scale(0.2)",
-        transformOrigin: "0 0",
-    }
-}));
+import "./page.scss"
 
 type PageProp = {
     element: XElement
@@ -64,16 +14,18 @@ type PageProp = {
 }
 
 const PageView: React.FunctionComponent<PageProp> = (props) => {
-    const classes = useStyles(props);
     const context = useContext(SlideContext);
     const mode = useProperty(context.state.presentMode);
+    const offset = Math.min(2, Math.max(-2, props.index - props.current));
     return (
         <Box
-            className={classes.wrapper + " " + (mode === "present" ? (classes.content + " " + classes.presentWrapper) : classes.outlineWrapper)}>
+            className={`wp-page-wrapper ${mode}`}>
             {mode === "present" ? (
-                renderChildren(props.element)
+                <Box className={`content offset-${offset}`}>
+                    {renderChildren(props.element)}
+                </Box>
             ) : (
-                <Paper className={classes.content + " " + classes.outlineContent} elevation={3}
+                <Paper className={"content"} elevation={3}
                        onClick={() => {
                            context.gotoPage(props.element);
                            context.state.presentMode.value = "present";
