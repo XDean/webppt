a6337a81329def9b8b63efdf60976e40b3c8c431
Dean Xu <373216024@qq.com>
2020-07-03 23:28:36 +0800

    toolbar css

--- frontend/src/components/tool/toolbar.tsx
+++ frontend/src/components/tool/toolbar.tsx
@@ -1,7 +1,5 @@
 import React, {useContext, useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import {Box, IconButton, Paper, Slide, Tooltip} from "@material-ui/core";
-import NavigatorView from "./navigator";
 import ViewModuleIcon from '@material-ui/icons/ViewModule';
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
@@ -9,46 +7,21 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 import LockIcon from '@material-ui/icons/Lock';
 import {SlideContext} from "../../model/context";
 import SlideshowIcon from '@material-ui/icons/Slideshow';
-import {ifClass, useProperty} from "../../util/util";
+import {useProperty} from "../../util/util";
+import "./toolbar.scss"
 
-const useStyles = makeStyles({
-    root: {
-        position: "fixed",
-        top: 20,
-        left: "50vw",
-        transform: "translateX(-50%)",
-        borderRadius: 5,
-        padding: "0px 5px",
-        zIndex: 100,
-    },
-    content: {
-        position: "relative",
-    },
-    button: {
-        textAlign: "center",
-        transition: "all 0.3s ease",
-        color: "white",
-        cursor: "pointer",
-        fontSize: 30,
-        "&:hover": {
-            backgroundColor: "#aaaaaa",
-        }
-    }
-});
-
-type ToolbarProp = {}
+type ToolbarProp = {};
 
 const ToolbarView: React.FunctionComponent<ToolbarProp> = (props) => {
-    const classes = useStyles();
     const context = useContext(SlideContext);
     const mode = useProperty(context.state.presentMode);
     const full = useProperty(context.state.fullScreen);
     const lock = useProperty(context.state.lockToolbar);
     const [show, setShow] = useState(false);
     return (
-        <Box className={classes.root} onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+        <Box className={"wp-toolbar"} onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
             <Slide in={show || lock}>
-                <Paper className={classes.content} elevation={3}>
+                <Paper className={"content"} elevation={3}>
                     {mode === "present" ? (
                         <Tooltip title={"Outline Mode"}>
                             <IconButton onClick={() => context.state.presentMode.value = "outline"}>
