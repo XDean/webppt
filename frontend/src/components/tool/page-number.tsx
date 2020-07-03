0660947c4f5b14a033c6831527c946014f9d189a
Dean Xu <373216024@qq.com>
2020-07-03 23:29:59 +0800

    pagenumber css

--- frontend/src/components/tool/page-number.tsx
+++ frontend/src/components/tool/page-number.tsx
@@ -1,28 +1,16 @@
 import React, {useContext} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import {Box} from "@material-ui/core";
 import {SlideContext} from "../../model/context";
 import {useProperty} from "../../util/util";
-
-const useStyles = makeStyles({
-    root: {
-        color: "#8c8c8c",
-        position: "absolute",
-        bottom: 5,
-        right: 20,
-        pointerEvents: "none",
-        zIndex: 100,
-    }
-});
+import "./page-number.scss"
 
 type PageNumberProp = {}
 
 const PageNumberView: React.FunctionComponent<PageNumberProp> = (props) => {
-    const classes = useStyles();
     const context = useContext(SlideContext);
     const current = useProperty(context.state.currentPage);
     return (
-        <Box className={classes.root}>
+        <Box className={"wp-page-number"}>
             {current + 1} / {context.pages.length}
         </Box>
     )
